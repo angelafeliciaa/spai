01e18c9780beafb7cd8f89b9291c117046efc18d
yyuri <yuriyasui012@gmail.com>
2025-01-18 15:23:36 -0800

    add requirements

--- llm/llama.py
+++ llm/llama.py
@@ -6,13 +6,18 @@ from huggingface_hub import login
 
 load_dotenv()
 
+HUGGINGFACE_TOKEN = os.getenv("HUGGINGFACE_TOKEN")
 login(token=HUGGINGFACE_TOKEN)
 
 model_id = "meta-llama/Llama-3.1-8B"
 
-pipeline = transformers.pipeline(
-    "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+text_generator = transformers.pipeline(
+    "text-generation",
+    model=model_id,
+    model_kwargs={"torch_dtype": torch.bfloat16},
+    device_map="auto"
 )
 
 if __name__ == "__main__":
-    pipeline("Hey how are you doing today?")
+    response = text_generator("Hey, how are you doing today?")
+    print(response[0]["generated_text"])
